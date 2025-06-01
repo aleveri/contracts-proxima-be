4b2c8f710702128ecd183fb9aee7af932ec0e189
Andres Leveri <aleveri@users.noreply.github.com>
2025-06-01 12:42:30 -0500

    Initial version of the script for entry-level tariffs

---
+++ insert_tarifas.sql
@@ -0,0 +1,7 @@
+-- Insert sample tarifas
+INSERT INTO tarifas (nombre, precio) VALUES
+('Tarifa Básica', 19.99),
+('Tarifa Estándar', 29.99),
+('Tarifa Premium', 49.99),
+('Tarifa Empresa', 99.99),
+('Tarifa Personalizada', 0.00);
